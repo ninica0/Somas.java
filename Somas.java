3d01ea58b1d477bffc97395205e6b0d9b795e386
ninica0 <santosnico048@gmail.com>
2024-10-04 15:15:43 -0300

    feat: Sistema que mostra a soma de todos os numeros entre 0 e 100

---
+++ Somas.java
@@ -0,0 +1,12 @@
+public class Somas {
+
+	public static void main(String[] args) {
+		int i, resultado;
+		resultado = 0;
+		for(i=0; i<=100; i ++) {
+			
+			resultado +=i;
+		}
+		System.out.println("O resultado das somas é: "+resultado);
+	}
+}
